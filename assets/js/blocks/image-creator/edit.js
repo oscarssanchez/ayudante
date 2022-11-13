e79017e12028e26ce94d9926d7c727428caba14c
Oscar Sanchez S <oscar@oscarssanchez.com>
2022-11-13 15:31:23 -0600

    Working version of DALLE implementation

--- assets/js/blocks/image-creator/edit.js
+++ assets/js/blocks/image-creator/edit.js
@@ -1,11 +1,35 @@
 import { Button, Placeholder, Modal } from '@wordpress/components';
+
 import { useState } from '@wordpress/element';
 
 const edit = () => {
 	const [isCreateImageModalOpen, setIsCreateImageModalOpen] = useState(false);
+	const [imageData, setImageData] = useState(null);
 
 	const openImageCreationModal = () => setIsCreateImageModalOpen(true);
 	const closeImageCreationModal = () => setIsCreateImageModalOpen(false);
+
+	const createImages = () => {
+		const input = document.getElementById('create-image-input');
+		const body = {
+			prompt: input.value,
+			n: 1,
+			size: '256x256',
+		};
+
+		fetch('https://api.openai.com/v1/images/generations', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer '
+			},
+			body: JSON.stringify(body),
+		}).then((response) => {
+			response.json().then((data) => {
+				setImageData(data);
+			});
+		});
+	};
 
 	return (
 		<>
@@ -15,10 +39,19 @@ const edit = () => {
 				</Button>
 			</Placeholder>
 			{isCreateImageModalOpen && (
-				<Modal title="Hello Modal" onRequestClose={closeImageCreationModal}>
-					<Button variant="secondary" onClick={closeImageCreationModal}>
-						Hello Modal
+				<Modal title="Let's Create an Image" onRequestClose={closeImageCreationModal}>
+					<input
+						id="create-image-input"
+						type="text"
+						placeholder="Type in some Ideas..."
+					/>
+					<Button variant="primary" onClick={createImages}>
+						Create Images
 					</Button>
+					{imageData &&
+						imageData.data.map((image) => {
+							return <img src={image.url} />;
+						})}
 				</Modal>
 			)}
 		</>
